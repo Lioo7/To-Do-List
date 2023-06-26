3c07cec84fe0a9db1f0e05737ef2b76b55213b36
Lioz Akirav <lioz.akirav@gmail.com>
2023-06-27 01:22:58 +0300

    add database

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,17 +10,76 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.get("/", function (req, res) {
+const itemsSchema = {
+    name: String
+};
 
-    let day = date.getDate();
+const Item = mongoose.model("item", itemsSchema);
 
+function initializeDatabase() {
+    const item1 = new Item({
+      name: "Welcome to your todolist!",
+    });
+  
+    const item2 = new Item({
+      name: "Hit the + button to add a new item.",
+    });
+  
+    const item3 = new Item({
+      name: "<-- hit this to delete an item.",
+    });
+  
+    const defaultItems = [item1, item2, item3];
+  
+    return new Promise((resolve, reject) => {
+      Item.insertMany(defaultItems)
+        .then(() => {
+          console.log("Successfully saved default items to DB");
+          resolve();
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }  
+
+function renderList(res, items) {
     res.render("list", {
-        listTitle: day,
+        listTitle: "Today",
         newListItems: items
     });
+}
 
+// Define a variable to keep track of whether the database has been initialized
+let isDatabaseInitialized = false;
+
+app.get("/", function (req, res) {
+  Item.find().then(function (foundItems) {
+    if (!isDatabaseInitialized || foundItems.length === 0) {
+      initializeDatabase()
+        .then(function () {
+          // Update the flag to indicate that the database has been initialized
+          isDatabaseInitialized = true;
+          Item.find().then(function (items) {
+            renderList(res, items);
+          });
+        })
+        .catch(function (err) {
+          console.log(err);
+          res.status(500).send("Error initializing database");
+        });
+    } else {
+      renderList(res, foundItems);
+    }
+  }).catch(function (err) {
+    console.log(err);
+    res.status(500).send("Error retrieving items from the database");
+  });
 });
+
 
 app.post("/", function (req, res) {
     let item = req.body.newItem;
